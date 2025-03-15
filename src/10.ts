72d351ec84761389d3a468883b55e39160512c63
subiabrenyat <subiabrenyat@gmail.com>
2025-03-15 05:55:53 +0000

    Add 10.ts

---
+++ src/10.ts
@@ -0,0 +1,16 @@
+// This is a TypeScript file
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
