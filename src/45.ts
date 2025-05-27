172c75005157e391eec75f952945eb1296da64a9
subiabrenyat <subiabrenyat@gmail.com>
2025-05-27 04:38:40 +0000

    Add 45.ts

---
+++ src/45.ts
@@ -0,0 +1,4 @@
+// This file contains a basic JavaScript code that defines a function named `addNumbers` with two parameters.
+function addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+}
